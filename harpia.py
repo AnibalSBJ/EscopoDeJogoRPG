a3852552515c7dfddf45546d2f94a74c0aba3f7e
AnibalSBJ <anibaljuniorbn@gmail.com>
2022-03-25 00:02:21 -0300

    adicionando mais um tipo de monstro ao jogo

---
+++ harpia.py
@@ -0,0 +1,9 @@
+class harpia:
+    def __init__(self,tamanho,cor,raridade,tipo,forca,level,vida):
+        self.tamanho = tamanho
+        self.cor = cor
+        self.raridade = raridade
+        self.tipo = tipo
+        self.forca = forca
+        self.level = level
+        self.vida = vida
